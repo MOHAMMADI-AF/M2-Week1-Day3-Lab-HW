9ceed6a128bcc46cb8ad62e3d8193c5406963177
Mohammad Akbar MOHAMMADI <MOHAMMADI@MOHAMMADI.ORG>
2022-11-30 13:52:34 -0500

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//Greetings
+//=========
 app.get("/", (req, res) => {
   res.send(`<h1>Hello, stranger</h1>`);
 });
@@ -9,6 +11,14 @@ app.get("/hello/:name", (req, res) => {
   res.send(`<h1>Wow! Hello There, </h1><h2>${req.params.name}</h2>`);
 });
 
+//Tip Calculator
+//==============
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+  const payAndTip = req.params.total * (req.params.tipPercentage / 100);
+  res.send(
+    `<h1>Thank You for your Payment </h1><h2>And your Tip of ${payAndTip} is appreciated!</h2>`
+  );
+});
 app.listen(port, () => {
   console.log(`App running at port ${port}`);
 });
